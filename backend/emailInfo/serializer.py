4e022ec339ee9bee9bf78a7f6f6b2f86df9f27fa
vanpersie68 <146914213+vanpersie68@users.noreply.github.com>
2023-11-12 03:48:06 +1100

    Delete backend directory

--- backend/emailInfo/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from emailInfo.models import EmailInfo
-
-class EmailInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EmailInfo
-        fields = ('id', 'sender', 'receiver', 'survey', 'accepted', 'rejected', 'expire_time')
--- backend/surveybuilder/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class SurveybuilderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'surveybuilder'
